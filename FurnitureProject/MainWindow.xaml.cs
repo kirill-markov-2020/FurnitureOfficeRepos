115e6d30210fe404f41e2d0361081601367e2437
kirill-markov-2020 <kirill-markov-2020@bk.ru>
2024-11-13 22:42:50 +0300

    Добавление подсказок для полей ввода логина и пароля.

--- FurnitureProject/MainWindow.xaml.cs
+++ FurnitureProject/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace FurnitureProject
         {
             InitializeComponent();
         }
+
+        private void TextBoxInputLogin_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxInputLogin.Text == "Введите логин")
+            {
+                TextBoxInputLogin.Text = "";
+                TextBoxInputLogin.Foreground = Brushes.Black;
+            }
+        }
+        private void TextBoxInputLogin_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TextBoxInputLogin.Text))
+            {
+                TextBoxInputLogin.Text = "Введите логин";
+                TextBoxInputLogin.Foreground = Brushes.Gray;
+            }
+        }
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordHintText.Visibility = Visibility.Collapsed;
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                PasswordHintText.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
