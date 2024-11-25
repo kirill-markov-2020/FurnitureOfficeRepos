75df47fd6f4857e7f4107013e6e9f372b0e4723a
kirill-markov-2020 <kirill-markov-2020@bk.ru>
2024-11-25 12:19:13 +0300

    Создание моделей данных

---
+++ FurnitureProject/Models/CategoryAndProductModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureProject.Models
+{
+
+
+    [Table("Category")] 
+
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+
+
+
+    [Table("Product")]
+
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryFurniture_Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
