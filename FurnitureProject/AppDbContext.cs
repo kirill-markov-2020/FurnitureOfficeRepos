c7ce5f11c44ee4abd9436534f3267e627d6bc4db
kirill-markov-2020 <kirill-markov-2020@bk.ru>
2024-11-25 12:21:27 +0300

    Создание класса контекста БД

---
+++ FurnitureProject/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FurnitureProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FurnitureProject
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=KIRILL-MARKOV;Database=DataBase;Integrated Security=True;");
+            }
+        }
+    }
+}
