37297dffcc6288c47a032c9107eb090bb21bc8bf
kirill-markov-2020 <kirill-markov-2020@bk.ru>
2024-11-28 16:22:03 +0300

    Распределение моделей по разным классам

---
+++ FurnitureProject/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureProject.Models
+{
+    [Table("Category")]
+
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+
+}
